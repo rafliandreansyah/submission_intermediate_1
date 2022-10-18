25955414d13b1645bd08d2a6fa3c7e774b5d93f5
rafli <rafli@mediainovasi.id>
2022-10-18 17:18:19 +0700

    [NEW] - Create XML Login And Register

--- app/src/main/java/com/dicoding/submission_intermediate_1/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/dicoding/submission_intermediate_1/ui/auth/LoginActivity.kt
@@ -1,8 +1,15 @@
 package com.dicoding.submission_intermediate_1.ui.auth
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.dicoding.submission_intermediate_1.databinding.ActivityLoginBinding
+import org.w3c.dom.Text
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,5 +20,45 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        hideActionBar()
+
+        binding.edLoginEmail.addTextChangedListener(watcher())
+        binding.edLoginPassword.addTextChangedListener(watcher())
+
+        binding.txtRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+    }
+
+    private fun hideActionBar() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun watcher() : TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.btnSignIn.isEnabled =
+                    binding.edLoginEmail.text.toString().trim().isNotEmpty() &&
+                            binding.edLoginEmail.error == null &&
+                            binding.edLoginPassword.text.toString().trim().isNotEmpty() &&
+                            binding.edLoginPassword.error == null
+            }
+
+        }
     }
 }
--- app/src/main/java/com/dicoding/submission_intermediate_1/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/submission_intermediate_1/ui/auth/RegisterActivity.kt
@@ -1,7 +1,10 @@
 package com.dicoding.submission_intermediate_1.ui.auth
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.dicoding.submission_intermediate_1.databinding.ActivityRegisterBinding
 import com.dicoding.submission_intermediate_1.model.UserModel
 
@@ -14,6 +17,20 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        hideActionBar()
 
+    }
+
+    private fun hideActionBar() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
